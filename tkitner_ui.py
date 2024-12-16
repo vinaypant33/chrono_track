82f8bbfad24ae21ecb8188ccbd9082ca1259a6fa
Vinay Pant <vinaypant24@gmail.com>
2024-12-17 04:03:44 +0530

    Initial UI Done  - Custom Controls - Closed and Minimize Button, Titlebar and app name

--- tkitner_ui.py
+++ tkitner_ui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import sys
 import threading as th
-
+from ctypes import windll
 
 
 ### Constants ###
@@ -12,10 +12,17 @@ controls_base_2 = '#1F1F1F'
 icons_front_color = '#A7A7A7'
 icons_front_color_2 = '#458DFF'
 other_icons_back_color = '#222B39'
-red_color  = "#F72C5B"
+red_color  = "#ff0000"
+white_color  = '#ffffff'
 
-app_width  = 600
-app_height  = 400
+
+app_width  = 400
+app_height  = 600
+minimized  = True
+main_x = 0 
+main_y = 0
+
+
 
 
 window  = tk.Tk()
@@ -37,19 +44,57 @@ window.geometry(f"{app_width}x{app_height}+{current_x_location}+{current_y_locai
 
 ###### Functions ######### 
 
+def set_appwindow(root):
+        GWL_EXSTYLE=-20
+        WS_EX_APPWINDOW=0x00040000
+        WS_EX_TOOLWINDOW=0x00000080
+        hwnd = windll.user32.GetParent(root.winfo_id())
+        style = windll.user32.GetWindowLongW(hwnd, GWL_EXSTYLE)
+        style = style & ~WS_EX_TOOLWINDOW
+        style = style | WS_EX_APPWINDOW
+        res = windll.user32.SetWindowLongW(hwnd, GWL_EXSTYLE, style)
+        root.wm_withdraw()
+        root.after(10, lambda: root.wm_deiconify())
+
 def close_application():
     window.destroy()
     sys.exit()
     
+def minimize_application():
+    window.overrideredirect(False)
+    window.wm_iconify()
+    window.bind('<FocusIn>' , on_deiconify)
 
+def on_deiconify(event):
+    if window.wm_state() =='normal' and window.overrideredirect() != True:
+            window.overrideredirect(True)
+            set_appwindow(window)
 
+def mouse_click(event):
+    # print(event)
+    global main_x
+    global main_y
+    main_x  = event.x
+    main_y = event.y
+
+def mouse_move(event):
+    # print(event)
+    global main_x 
+    global main_y 
+    main_x = main_x
+    main_y = main_y
+    deltax = event.x - main_x
+    deltay = event.y - main_y
+    x_location = window.winfo_x() + deltax
+    y_location  = window.winfo_y() + deltay
+    window.geometry(f"{app_width}x{app_height}+{x_location}+{y_location}")
 
 
 #### Title bar and related buttons ( minimize and close )
 title_bar  = tk.Frame(window , height=30 , width=app_width , background=controls_base)
 close_button  = tk.Button(title_bar , text='\u2716' , command=close_application)
-
-
+minimized_button  =tk.Button(title_bar , text=u'\u2014' , command=minimize_application )
+application_name  = tk.Label(title_bar , text  = "Chrono Track" )
 
 
 
@@ -61,21 +106,27 @@ def button_styles(master , height , width , background , foreground , activebg ,
 window.configure(background=application_base)
 title_bar.pack_propagate(False)
 # button_styles(close_button , 1 , 1 , icons_front_color , None , None , None , 0 )
-close_button.configure(background=controls_base , foreground=icons_front_color , relief='flat' , bd=0 , activebackground=red_color , activeforeground=icons_front_color_2 )
-
+close_button.configure(background=controls_base , foreground=icons_front_color , relief='flat' , bd=0 , activebackground=red_color , activeforeground=white_color )
+minimized_button.configure(background=controls_base , foreground=icons_front_color , relief='flat' , bd = 0 , activebackground=other_icons_back_color , activeforeground=white_color)
+application_name.configure(background=controls_base , foreground=white_color)
 
 
 
 
 ##### Binding the controls ##### 
-
-
+title_bar.bind("<ButtonPress-1>" , mouse_click)
+title_bar.bind("<B1-Motion>" , mouse_move)
+close_button.bind("<Enter>" , lambda x : close_button.configure(background=red_color))
+close_button.bind("<Leave>" , lambda x  :  close_button.configure(background=controls_base))
+minimized_button.bind("<Enter>" , lambda x : minimized_button.configure(background=other_icons_back_color))
+minimized_button.bind("<Leave>" , lambda x  : minimized_button.configure(background=controls_base))
 
 
 ##### Packing the controls #### 
 title_bar.pack()
-close_button.pack(side=tk.RIGHT ,padx=(0,0))
-
+close_button.pack(side=tk.RIGHT ,padx=(0,0) , fill=tk.Y)
+minimized_button.pack(side=tk.RIGHT , padx=(0 , 0) , fill=tk.Y)
+application_name.pack(side = tk.LEFT , padx=(5 , 0))
 
 
 
